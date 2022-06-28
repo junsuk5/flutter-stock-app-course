d66b46855c6f13de9b936eb06adaa09263f27a29
Junsuk Oh <junsuk.oh@survivalcoding.com>
2022-06-28 13:29:40 +0900

    mapper

---
+++ lib/data/mapper/company_mapper.dart
@@ -0,0 +1,22 @@
+import 'package:stock_app/data/source/local/company_listing_entity.dart';
+import 'package:stock_app/domain/model/company_listing.dart';
+
+extension ToCompanyListing on CompanyListingEntity {
+  CompanyListing toCompanyListing() {
+    return CompanyListing(
+      symbol: symbol,
+      name: name,
+      exchange: exchange,
+    );
+  }
+}
+
+extension ToCompanyListingEntity on CompanyListing {
+  CompanyListingEntity toCompanyListingEntity() {
+    return CompanyListingEntity(
+      symbol: symbol,
+      name: name,
+      exchange: exchange,
+    );
+  }
+}

b69b00b0beacbfd4fb776d2639b37b5495a298b1
Junsuk Oh <junsuk.oh@survivalcoding.com>
2022-06-28 18:27:31 +0900

    회사 정보 ViewModel

---
+++ lib/presentation/company_info/company_info_state.dart
@@ -0,0 +1,16 @@
+import 'package:freezed_annotation/freezed_annotation.dart';
+import 'package:stock_app/domain/model/company_info.dart';
+
+part 'company_info_state.freezed.dart';
+
+part 'company_info_state.g.dart';
+
+@freezed
+class CompanyInfoState with _$CompanyInfoState {
+  const factory CompanyInfoState({
+    CompanyInfo? companyInfo,
+    @Default(false) bool isLoading,
+  }) = _CompanyInfoState;
+
+  factory CompanyInfoState.fromJson(Map<String, Object?> json) => _$CompanyInfoStateFromJson(json);
+}
---
+++ lib/presentation/company_info/company_info_view_model.dart
@@ -0,0 +1,36 @@
+import 'package:flutter/material.dart';
+import 'package:stock_app/domain/repository/stock_repository.dart';
+import 'package:stock_app/presentation/company_info/company_info_state.dart';
+
+class CompanyInfoViewModel with ChangeNotifier {
+  final StockRepository _repository;
+
+  var _state = const CompanyInfoState();
+
+  CompanyInfoState get state => _state;
+
+  CompanyInfoViewModel(this._repository);
+
+  Future<void> loadCompanyInfo(String symbol) async {
+    _state = state.copyWith(isLoading: true);
+    notifyListeners();
+
+    final result = await _repository.getCompanyInfo(symbol);
+    result.when(
+      success: (info) {
+        _state = state.copyWith(
+          companyInfo: info,
+          isLoading: false,
+        );
+      },
+      error: (e) {
+        _state = state.copyWith(
+          companyInfo: null,
+          isLoading: false,
+        );
+      },
+    );
+
+    notifyListeners();
+  }
+}
